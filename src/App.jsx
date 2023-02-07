9b7ea9bcbb06a0a2e8cc612f92dc7ae54cff6a79
mrima <mrimamss@gmail.com>
2023-02-07 13:22:23 +0300

    import boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Box1 from "./components/Box1.jsx";
+import Box2 from "./components/Box2.jsx";
+import Box3 from "./components/Box3.jsx";
+import Box4 from "./components/Box4.jsx";
+import Box5 from "./components/Box5"
 import './App.css'
 
-function App() {
+export default function App() {
 
   return (
-    <>
-    </>
+    <div className={"p-20"}>
+
+        <Box4/>
+    </div>
   )
 }
 
-export default App
